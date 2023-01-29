a71f1b28224a757beb3148da12915906835e33ed
Ahmed Salem <superahmed208@gmail.com>
2023-01-29 15:31:51 -0800

    Fixed RPS

--- rps.py
+++ rps.py
@@ -1,52 +1,37 @@
 import random
 
-win=False
+win='none'
 while True:
-    player=input('Type "r" "p" or "s" ')
-    if player=='r' or player=='p' or player=='s':
+    player=input('Type "rock" "paper" or "scissors" ')
+    if player=='rock' or player=='paper' or player=='scissors':
         cpu=random.randint(0, 2)
-        if player=='r':
-            player=0
-        elif player=='p':
-            player=1
-        else:
-            player=2
-
-        if player==0 and cpu==2:
-            win==True
-        elif player==2 and cpu==0:
-            win==False
-        elif player==cpu:
-            win==0
-        elif player>cpu:
-            win=True
-        
-        if player==0:
-            player='rock'
-        elif player==1:
-            player='paper'
-        else:
-            player='scissors'
-        
         if cpu==0:
             cpu='rock'
         elif cpu==1:
             cpu='paper'
         else:
             cpu='scissors'
-    
+        
         print(f'You played {player}.')
-        print(f'CPU played {cpu}.')
+        print(f'CPU played {cpu}.')   
 
-        if win:
-            print('You won!')
-        elif win==0:
+        if player==cpu:
             print("It's a draw.")
-        else:
+        elif player=='rock' and cpu=='paper':
             print('You lost :(')
+        elif player=='rock' and cpu=='scissors':
+            print('You won!')
+        elif player=='paper' and cpu=='rock':
+            print('You won!')
+        elif player=='paper' and cpu=='scissors':
+            print('You lost :(')
+        elif player=='scissors' and cpu=='rock':
+            print('You lost :(')
+        elif player=='scissors' and cpu=='paper':
+            print('You won!')
         
     elif player.lower()=='exit':
         break
     else:
-        print('This is not a valid input, please try again.')
+        print('That is not a valid input, please try again.')
         continue
