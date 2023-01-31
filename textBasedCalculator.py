d12b80d7b19ab311d8cb8bc6372776f424235dde
Ahmed Salem <superahmed208@gmail.com>
2023-01-30 21:14:19 -0800

    Added non-functional calculator

---
+++ textBasedCalculator.py
@@ -0,0 +1,8 @@
+import math
+
+print('Hi!, welcome to the text based calculator. Type "help" to get a list of commands.')
+while True:
+    userInput=input('> ')
+
+    if userInput.lower()=='exit':
+        break
